96718e2276c3b5599cf9b32d9ee41a16491931d2
Nguyễn Đăng Thế <dangthe1941988@gmail.com>
2019-03-06 22:31:20 +0700

    First Commit

---
+++ FullHouse/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FullHouse.Startup))]
+namespace FullHouse
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
